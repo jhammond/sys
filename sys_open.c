bfb2ee08859f89154ea5cb6d7b7f0dd8463f62fb
John L. Hammond <jhammond@tacc.utexas.edu>
2012-06-15 10:04:18 -0500

    Add options to usage in sys_open.

--- sys_open.c
+++ sys_open.c
@@ -9,7 +9,15 @@
 
 static void usage(FILE *file, int status)
 {
-  fprintf(file, "Usage: %s [OPTION]... PATH FLAGS\n",
+  fprintf(file, "Usage: %s [OPTION]... PATH FLAGS\n"
+          "Open file.\n"
+          "\n"
+          "Mandatory arguments to long options are mandatory for short options too.\n"
+          "  -h, --help\n"
+          "  -m, --mode=MODE\n"
+          "  -p, --pause\n"
+          "  -s, --sleep=SECONDS\n"
+          ,
 	  program_invocation_short_name);
   exit(status);
 }
